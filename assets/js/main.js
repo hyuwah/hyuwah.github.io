bd4f62ad40a742c14bfe3cfbdadd571535364050
Muhammad Wahyudin <fnp90spy@gmail.com>
2016-02-19 18:23:08 +0700

    fixed broken overlay on medium and down screen, fixed some js not loaded

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@
  * http://hyuwah.github.io/
  */
 
+// TODO:20 optimisasi skrip transisi page
+// FIXME: Waktu load pertama atau random, skrip materializenya ga aktif, kalo reload baru bisa
+//
+//
  // Contents of functions.js
  $(function(){
  'use strict';
@@ -12,7 +16,7 @@
       prefetch: true,
       cacheLength: 2,
       onStart: {
-        duration: 250, // Duration of our animation
+        duration: 1500, // Duration of our animation
         render: function ($container) {
           // Add your CSS animation reversing class
           $container.addClass('is-exiting');
@@ -28,34 +32,7 @@
           // Inject the new content
           $container.html($newContent);
           // Materialize jquery initialization
-          $(".button-collapse").sideNav({
-              edge: 'right', // Choose the horizontal origin
-            });
-          $('.parallax').parallax();
-          $('.scrollspy').scrollSpy();
-          $('#sticky').pushpin({ top:0 });
-          $('.carousel').carousel();
-          $('.collapsible').collapsible({accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-          });
-          $('.slider').slider({full_width: false});
-
-          // Go Up smoothscroll
-          $('#goup').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 600);
-            return false;
-           });
-
-           // Raised navbar
-          $(window).scroll(function() {
-            var y = $(window).scrollTop();
-            if (y > 0) {
-              $("#navbar").removeClass('z-depth-0').addClass('z-depth-1');
-            } else if (y==0) {
-              $("#navbar").removeClass('z-depth-1').addClass('z-depth-0');
-            } else {
-              $("#navbar").removeClass('z-depth-0').addClass('z-depth-1');
-            }
-          });
+          paket();
 
         }
       }
@@ -63,8 +40,37 @@
     smoothState = $page.smoothState(options).data('smoothState');
     });
 
- //  $(document).ready(function() {
- //
- //
- //
- // });
+  $(document).ready(function() {
+    paket();
+ });
+
+var paket = function(){
+  $(".button-collapse").sideNav({
+      edge: 'right', // Choose the horizontal origin
+    });
+  $('.parallax').parallax();
+  $('.scrollspy').scrollSpy();
+  $('#sticky').pushpin({ top:0 });
+  $('.carousel').carousel();
+  $('.collapsible').collapsible({accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+  $('.slider').slider({full_width: false});
+
+  // Go Up smoothscroll
+  $('#goup').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+   });
+
+   // Raised navbar
+  $(window).scroll(function() {
+    var y = $(window).scrollTop();
+    if (y > 0) {
+      $("#navbar").removeClass('z-depth-0').addClass('z-depth-1');
+    } else if (y==0) {
+      $("#navbar").removeClass('z-depth-1').addClass('z-depth-0');
+    } else {
+      $("#navbar").removeClass('z-depth-0').addClass('z-depth-1');
+    }
+  });
+};
